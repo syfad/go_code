fabc55c24046dd45ce85d25b817628c864c231f9
“syfad” <sunyfad@gmail.com>
2020-12-03 01:15:54 +0800

    20201203

--- 06day/data_format.go
+++ 06day/data_format.go
@@ -5,41 +5,15 @@ import (
 	"fmt"
 	"github.com/axgle/mahonia"
 	"io"
-	"io/ioutil"
 	"os"
 	"regexp"
-	"strings"
 )
 
 var reIDcard = `.*[1-9]\d{5}((19\d{2})|(20[0-2]\d))((0[1-9])|(1[0-2]))((0[1-9])|(1\d)|(2\d)|(3[01]))\d{3}[\dXx].*`
 
 
-//var writeChan chan string
-//var readChan chan string
-
 func main() {
-	writeChan := make(chan string)
-	readChan := make(chan string)
-	read2(writeChan)
-	createPool(256, writeChan, readChan)
-
-	file, err := os.OpenFile("format_after.txt", os.O_CREATE|os.O_TRUNC|os.O_WRONLY, 0644)
-	if err != nil {
-		fmt.Println("open file failed, err:", err)
-		return
-	}
-
-	defer file.Close()
-
-	for read := range readChan {
-		write := bufio.NewWriter(file)
-		write.WriteString(read)
-		write.Flush()
-	}
-
-
-
-	//createPoll(256, writeChan, readChan)
+	read2()
 }
 
 
@@ -48,18 +22,7 @@ func HandleError(err error)  {
 		fmt.Println(err)
 	}
 }
-func read()  {
-	conntentBytes, err := ioutil.ReadFile("/Users/sun_admin/Desktop/go7期/数据/省份.txt")
-	HandleError(err)
-	conntenStr := string(conntentBytes)
-	lineStrs := strings.Split(conntenStr, "\n\r")
-	for _, lineStr := range lineStrs{
-		fmt.Println(lineStr)
-		newStr := ConvertEncoding(lineStr, "GBk")
-		fmt.Println(newStr)
-	}
 
-}
 
 func ConvertEncoding(srcStr string, encoding string)(dstStr string){
 	enc := mahonia.NewDecoder(encoding)
@@ -67,37 +30,35 @@ func ConvertEncoding(srcStr string, encoding string)(dstStr string){
 	return
 }
 
-func read2(c chan string)  {
-	//file, err := os.Open("/Users/sun_admin/Desktop/go7期/数据/kaifang.txt")
-	file, err := os.Open("/Users/sun_admin/Desktop/go7期/shot.txt")
-	defer file.Close()
+func read2()  {
+	file, err := os.Open("/Users/sun_admin/Desktop/go7期/数据/kaifang.txt")
 	HandleError(err)
+	//file, err := os.Open("/Users/sun_admin/Desktop/go7期/shot.txt")
+	defer file.Close()
 	reader := bufio.NewReader(file)
+
+	file1, err1 := os.OpenFile("format_after.txt", os.O_CREATE|os.O_TRUNC|os.O_WRONLY, 0644)
+	HandleError(err1)
+	defer file1.Close()
+
 	for {
 		lineBytes, _, err := reader.ReadLine()
 		if err == io.EOF{
 			break
 		}
 		myStr := string(lineBytes)
-		//xxStr := ConvertEncoding(myStr, "GBK")
+		xxStr := ConvertEncoding(myStr, "GBK")
 		//fmt.Println(myStr)
 		re := regexp.MustCompile(reIDcard)
-		results := re.FindAllStringSubmatch(myStr, -1)
+		results := re.FindAllStringSubmatch(xxStr, -1)
+		//results := re.FindAllStringSubmatch(myStr, -1)
 		for _, res := range results{
 			//fmt.Println(res[0])
-			c <- res[0]
+
+			w_lin := bufio.NewWriter(file1)
+			w_lin.WriteString(res[0]+"\n")
+			w_lin.Flush()
+			}
 		}
 		//fmt.Println(results[1])
 	}
-
-}
-
-func createPool(num int, writeChan chan string, readChan chan string)  {
-	for i :=0; i<num;i++{
-		go func(writeChan chan string, readChan chan string) {
-			for writeline := range writeChan{
-				readChan <- writeline
-			}
-		}(writeChan, readChan)
-	}
-}
